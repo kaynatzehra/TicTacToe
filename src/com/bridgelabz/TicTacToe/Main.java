1c9eb0b0aaeeedc67b9908ca6601d99095fccc79
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-03-08 13:11:24 +0530

    Added: Creating Tic Tac Toe Board

--- src/com/bridgelabz/TicTacToe/Main.java
+++ src/com/bridgelabz/TicTacToe/Main.java
@@ -1,8 +1,13 @@
 package com.bridgelabz.TicTacToe;
 
+import static com.bridgelabz.TicTacToe.tictactoe.ticTacToe;
+
 public class Main {
 
-        public static void main(String[] args) {
+        public static void main(String[] args)
+        {
             System.out.println("Tic Tac Toe problem");
+
+            ticTacToe();
         }
-    }
+        }
