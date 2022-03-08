b1a0d07b62f57e6677bbe4b53e03f0cfecbaeb65
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-03-08 14:58:43 +0530

    Added: Toss to check who plays first

--- src/com/bridgelabz/TicTacToe/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe/TicTacToe.java
@@ -1,25 +1,30 @@
 package com.bridgelabz.TicTacToe;
 
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
-    public static char[] gameArray = new char[10];
     private static char userTurn;
     private static char computerTurn;
     public static int userPos;
     public static int computerPos;
+    public static char[] gameArray = new char[10];
     public static Scanner scanner = new Scanner(System.in);
+
 
     public static void ticTacToe() {
         for (int i = 0; i < gameArray.length; i++) {
+
             gameArray[i] = ' ';
         }
     }
+
     public static char user(Scanner scanner) {
         System.out.println("Enter your choice  X or O only");
         return scanner.next().toUpperCase().charAt(0);
     }
+
     public static void showBoard() {
 
         System.out.println(gameArray[0] + " | " + gameArray[1] + " | " + gameArray[2]);
@@ -27,7 +32,7 @@ public class TicTacToe {
         System.out.println(gameArray[6] + " | " + gameArray[7] + " | " + gameArray[8]);
     }
 
-    public static void userLocation() {
+    public static void moveLocation() {
         System.out.print("\nSelect the position in board from range [0 - 8] : ");
         userPos = scanner.nextInt();
 
@@ -168,12 +173,29 @@ public class TicTacToe {
 
         }
     }
+
     public static void main(String[] args) {
         ticTacToe();
         userTurn = user(scanner);
         computerTurn = (userTurn == 'X') ? 'O' : 'X';
         showBoard();
-        userLocation();
-        comLocation();
+        int Toss;
+        int wonToss;
+        System.out.println("Choose\n 0. Heads\n 1. Tails\nEnter you choice [0-1] : ");
+        Toss = scanner.nextInt();
+        Random rand1 = new Random();
+        wonToss = rand1.nextInt(2);
+
+        if (wonToss == Toss) {
+            moveLocation();
+            showBoard();
+            comLocation();
+            showBoard();
+        }else{
+            comLocation();
+            showBoard();
+            moveLocation();
+            showBoard();
+        }
     }
 }
